be861dbcec975c863b07e2ea064cd6a7d26ed658
xiejay <xiejay97@gmail.com>
2024-05-30 18:25:31 +0800

    feat(http): support override all configs

--- packages/http/src/axios.ts
+++ packages/http/src/axios.ts
@@ -11,14 +11,14 @@ export function axios(config: AxiosRequestConfig<any>) {
 
   const headers: any = Object.assign({}, config.headers);
   if (token && !isNull(token.value)) {
-    headers.Authorization = `Bearer ${token.value}`;
+    headers.Authorization = headers.Authorization || `Bearer ${token.value}`;
   }
 
   return axios({
     ...config,
-    baseURL: CONFIGS.baseURL,
+    baseURL: config.baseURL || CONFIGS.baseURL,
     // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
-    url: CONFIGS.transformURL(config.url!),
+    url: config.url || CONFIGS.transformURL(config.url!),
     headers,
   });
 }
