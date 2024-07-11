fc533819c35edc22a4a870600256014666b54264
xiejay <xiejay97@gmail.com>
2024-07-11 17:25:41 +0800

    fix(acl): fix acl update

--- packages/acl/src/useACL.ts
+++ packages/acl/src/useACL.ts
@@ -3,9 +3,14 @@ import type { Control, ControlMode } from './types';
 import { isArray } from 'lodash';
 import { useSyncExternalStore } from 'react';
 
+let _full = false;
+let _controls = new Set<Control>();
+
+let controls: Control[] = [];
 let listeners: (() => void)[] = [];
 
 function emitChange() {
+  controls = Array.from(_controls);
   for (const listener of listeners) {
     listener();
   }
@@ -18,50 +23,49 @@ function subscribe(onStoreChange: () => void) {
   };
 }
 
-let _full = false;
-let _controls = new Set<Control>();
 function getSnapshot() {
-  return {
-    get controls() {
-      return Array.from(_controls);
-    },
-    setFull: (full: boolean) => {
-      _full = full;
-      emitChange();
-    },
-    set: (control: Control[]) => {
-      _controls = new Set(control);
-      emitChange();
-    },
-    add: (control: Control | Control[]) => {
-      for (const v of isArray(control) ? control : [control]) {
-        _controls.add(v);
-      }
-      emitChange();
-    },
-    remove: (control: Control | Control[]) => {
-      for (const v of isArray(control) ? control : [control]) {
-        _controls.delete(v);
-      }
-      emitChange();
-    },
-    can: (control: Control | Control[], mode: ControlMode = 'one') => {
-      if (_full) {
-        return true;
-      }
-
-      const controls = isArray(control) ? control : [control];
-      if (mode === 'one') {
-        return controls.some((v) => _controls.has(v));
-      } else {
-        return controls.every((v) => _controls.has(v));
-      }
-    },
-  };
+  return controls;
 }
 
-export const acl = getSnapshot();
+export const acl = {
+  get controls() {
+    return controls;
+  },
+  setFull: (full: boolean) => {
+    _full = full;
+    emitChange();
+  },
+  set: (control: Control[]) => {
+    _controls = new Set(control);
+    emitChange();
+  },
+  add: (control: Control | Control[]) => {
+    for (const v of isArray(control) ? control : [control]) {
+      _controls.add(v);
+    }
+    emitChange();
+  },
+  remove: (control: Control | Control[]) => {
+    for (const v of isArray(control) ? control : [control]) {
+      _controls.delete(v);
+    }
+    emitChange();
+  },
+  can: (control: Control | Control[], mode: ControlMode = 'one') => {
+    if (_full) {
+      return true;
+    }
+
+    const controls = isArray(control) ? control : [control];
+    if (mode === 'one') {
+      return controls.some((v) => _controls.has(v));
+    } else {
+      return controls.every((v) => _controls.has(v));
+    }
+  },
+};
 
 export function useACL() {
-  return useSyncExternalStore(subscribe, getSnapshot, getSnapshot);
+  useSyncExternalStore(subscribe, getSnapshot, getSnapshot);
+  return acl;
 }
