9216b1facbf2854de7509934ba0c6b7931267083
xiejay <xiejay97@gmail.com>
2024-09-19 12:02:01 +0800

    refactor(router): replace token with expired in useTokenGuard

--- packages/router/src/useTokenGuard.ts
+++ packages/router/src/useTokenGuard.ts
@@ -1,13 +1,11 @@
 import type { CanActivateFn } from './types';
-import type { Token } from '@laser-pro/auth';
 
-import { isNull } from 'lodash';
 import { createElement } from 'react';
 import { Navigate } from 'react-router-dom';
 
-export function useTokenGuard(token: Token, redirect: string, state?: any): CanActivateFn {
+export function useTokenGuard(expired: boolean, redirect: string, state?: any): CanActivateFn {
   return () => {
-    if (isNull(token.value) || token.expired) {
+    if (expired) {
       return createElement(Navigate, {
         to: redirect,
         state,
