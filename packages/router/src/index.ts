b815a3a9b881fd266e7a7dfc50cdf62d4ed68eef
xiejay97 <xiejay97@gmail.com>
2026-03-27 10:04:04 +0800

    refactor(router): remove `createReuseOutlet`

--- packages/router/src/index.ts
+++ packages/router/src/index.ts
@@ -1,5 +1,4 @@
 export { Router } from './Router';
-export { createReuseOutlet } from './ReuseOutlet';
 export { useQueryParams } from './useQueryParams';
 export { useACLGuard } from './useACLGuard';
 export { useTokenGuard } from './useTokenGuard';
